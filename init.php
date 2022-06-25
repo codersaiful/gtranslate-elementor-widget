1602fccb07fa2ddb23ba39592d76116043528bbd
Saiful Islam <codersaiful@gmail.com>
2022-06-25 11:50:22 +0600

    reqired plugin notice added

--- init.php
+++ init.php
@@ -15,6 +15,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 function elementor_gt_addon() {
+	
+	if( ! class_exists('gtranslate') || ! did_action( 'elementor/loaded' ) ){
+		add_action( 'admin_notices', 'elementor_gt_addon_notice_root' );
+		return;
+	}
 
 	// Load plugin file
 	require_once( __DIR__ . '/includes/plugin.php' );
@@ -24,3 +29,7 @@ function elementor_gt_addon() {
 
 }
 add_action( 'plugins_loaded', 'elementor_gt_addon' );
+
+function elementor_gt_addon_notice_root(){
+	echo '<div class="error error-warning is-dismissible"><p><strong>GTranslate & Elementor</strong> reuire for <strong>Gtranslate Elementor Widget</strong> </p></div>';
+}
